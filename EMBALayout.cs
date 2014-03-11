40de24abcf3150e4a69893e4d3897c198a786d42
jungfengpaulwang <paul.wang@ischool.com.tw>
2014-03-11 21:44:00 +0800

    no message

---
+++ EMBALayout.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FISCA;
+using System.Xml.Linq;
+
+namespace UIOrder
+{
+    class EMBALayout : ILayoutProvider
+    {
+        #region ILayoutProvider 成員
+
+        public System.Xml.Linq.XElement LayoutDescription
+        {
+            get {
+                return XElement.Parse(Properties.Resources.LayoutDescription);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using FISCA;
+
+namespace UIOrder
+{
+    /// <summary>
+    /// 設定 UI 的 順序，包含 DetailContent, RibbonBarItem, ....
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [MainMethod()]
+        public static void Main()
+        {
+            CustomizationService.Register<ILayoutProvider>(new EMBALayout());
+        }
+    }
+}
